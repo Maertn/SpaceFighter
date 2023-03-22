f711a20570f47f1f3c3ddf927a29a1a195e42c02
maertn <maartenbouwmans@gmail.com>
2023-03-22 19:10:51 +0100

    enemy movement

--- enemies.py
+++ enemies.py
@@ -3,14 +3,34 @@ import pygame as pg
 class Enemy(pg.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pg.Surface((32,32)).convert_alpha()
+        self.image = pg.Surface((48,48)).convert_alpha()
         self.image.fill('blue')
         self.rect = self.image.get_rect(center = pos)
 
+        # movement
+        self.direction = pg.math.Vector2()
+        self.speed = 5
+
+    def line_move(self):
+        self.direction = (-1, 0)
+        self.rect.x += self.direction[0] * self.speed
+
+    def bouncy_move(self):
+        if self.rect.centerx <= 0 or self.direction == (0,0):
+            self.direction = ((1,0))
+        elif self.rect.centerx >= 1280:
+            self.direction = ((-1,0))
+        else:
+            pass
+
+        self.rect.x += self.direction[0] * self.speed
+
     def destroy_enemy(self):
         if self.rect.centerx <= -50 or self.rect.centerx >= 1300:
+            print('enemy died by boundaries')
             self.kill()
 
     def update(self):
+        self.bouncy_move()
         self.destroy_enemy()       
         
--- level.py
+++ level.py
@@ -1,4 +1,6 @@
 import pygame as pg
+import time
+
 from settings import *
 from player import Player
 from bullets import Bullet
@@ -17,11 +19,24 @@ class Level:
         # sprite setup
         self.create_map()
 
+        # enemy spawning
+        self.enemy_cooldown = False 
+
     def create_map(self):
         x = WIDTH // 2
         y = HEIGHT // 2
         self.player = Player((x,y), [self.visible_sprites])
-        self.enemy = Enemy((x, y - 300), [self.visible_sprites])
+        self.enemy = Enemy((WIDTH, HEIGHT // 2 - 300), [self.visible_sprites])
+
+    # def spawn_enemies(self):
+    #     if not self.enemy_cooldown:
+    #         i = time.perf_counter()
+    #         if int(i % 10) == 0:
+    #             self.enemy_cooldown_setter()
+    #             if int(i % 10) == 1:
+    #                 self.enemy_cooldown = False
+
+    
 
     def shoot_stuff(self, player):
         if self.player.shooting and not self.player.dodging:
@@ -33,6 +48,7 @@ class Level:
 
     def run(self):
         self.shoot_stuff(self.player)
+        # self.spawn_enemies()
         self.visible_sprites.draw(self.display_surface)
         self.visible_sprites.update()
         
